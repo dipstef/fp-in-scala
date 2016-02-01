4cdcc84a734c30de939e1194f17c78ae9b4b9d6b
Stefano <stefano.dipierro@duedil.com>
2016-02-01 18:13:39 +0000

    Chapter6: Exercise 11

---
+++ src/chapter06/CandyMachine.scala
@@ -0,0 +1,8 @@
+package chapter06
+
+/**
+  * Created by dipstef on 01/02/2016.
+  */
+object CandyMachine {
+
+}
---
+++ src/chapter06/exercises/Exercise11.scala
@@ -0,0 +1,8 @@
+package chapter06.exercises
+
+/**
+  * Created by dipstef on 01/02/2016.
+  */
+object Exercise11 {
+
+}
