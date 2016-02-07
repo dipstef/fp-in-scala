61f3255ca3d9d1f21d28ea347b579a1ff76c61f7
Stefano <dipstef@gmail.com>
2016-02-07 21:21:06 +0000

    updated example

--- src/chapter07/Examples.scala
+++ src/chapter07/Examples.scala
@@ -8,18 +8,36 @@ trait ParallelComputation {
 
   def get[A](a: Par[A]): A
 
+  def map2[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C]
+
 }
 
 abstract class ParallelSum extends ParallelComputation {
 
-  def sum(ints: IndexedSeq[Int]): Int =
+  def sumSequential(ints: IndexedSeq[Int]): Int =
     if (ints.size <= 1)
       ints.headOption getOrElse 0
     else {
       val (l, r) = ints.splitAt(ints.length / 2)
-      val sumL: Par[Int] = unit(sum(l))
-      val sumR: Par[Int] = unit(sum(r))
+      val sumL: Par[Int] = unit(sumSequential(l))
+      val sumR: Par[Int] = unit(sumSequential(r))
+      // This actually should avoided as get forces the side effects to happen, and as such we can't combine
+      // parallel computations, as in this case get(sumL) will block before we can call get(sumR)
       get(sumL) + get(sumR)
+    }
+
+  // Because map2 is strict, and Scala evaluates arguments left to right, whenever we encounter map2(sum(x),sum(y))(_ + _),
+  // we have to then evaluate sum(x) and so on recursively.
+  // This has as consequence that we’ll strictly construct the entire left half of the tree of summations first before
+  // moving on to (strictly) constructing the right half
+
+  def sum(ints: IndexedSeq[Int]): Par[Int] =
+    if (ints.size <= 1)
+      unit(ints.headOption getOrElse 0)
+    else {
+      val (l, r) = ints.splitAt(ints.length / 2)
+
+      map2(sum(l), sum(r))(_+_)
     }
 
 }
