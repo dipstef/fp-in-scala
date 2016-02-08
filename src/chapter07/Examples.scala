258d8f1cd8b1e86f3aa9b72e110d0d28f22ada3c
Stefano <stefano.dipierro@duedil.com>
2016-02-08 00:10:22 +0000

    refactored examples

--- src/chapter07/Examples.scala
+++ src/chapter07/Examples.scala
@@ -1,7 +1,20 @@
 package chapter07
 
 
-abstract class ParallSumSequential extends ParallelComputationRun {
+object SumSequential {
+  // `IndexedSeq` is a superclass of random-access sequences like `Vector` in the standard library. Unlike lists,
+  // these sequences provide an efficient `splitAt` method for dividing them into two parts at a particular index.
+  def sum(ints: IndexedSeq[Int]): Int =
+    if (ints.size <= 1)
+      ints.headOption getOrElse 0
+    else {
+      val (l, r) = ints.splitAt(ints.length / 2)
+      sum(l) + sum(r)
+    }
+
+}
+
+abstract class ParallelSumSequential extends ParallelComputationRun {
   def sum(ints: IndexedSeq[Int]): Int =
     if (ints.size <= 1)
       ints.headOption getOrElse 0
@@ -66,21 +79,11 @@ abstract class ParallelSum extends ParallelComputation {
 
 object Examples {
 
-  // `IndexedSeq` is a superclass of random-access sequences like `Vector` in the standard library. Unlike lists,
-  // these sequences provide an efficient `splitAt` method for dividing them into two parts at a particular index.
-  def sum(ints: IndexedSeq[Int]): Int =
-    if (ints.size <= 1)
-      ints.headOption getOrElse 0
-    else {
-      val (l, r) = ints.splitAt(ints.length / 2)
-      sum(l) + sum(r)
-    }
-
   def main(args: Array[String]) {
     val v = Vector(1, 2, 3, 4, 5)
 
     println("v = " + v)
-    println("sum(v) = " + sum(v))
+    println("sum(v) = " + SumSequential.sum(v))
   }
 
 }
