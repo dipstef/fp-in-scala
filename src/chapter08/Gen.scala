6d55e8f7bd53e721e39d44479fcea3a937119664
Stefano <stefano.dipierro@duedil.com>
2016-06-12 15:02:57 +0100

    Chapter 8 exercise 4

--- src/chapter08/Gen.scala
+++ src/chapter08/Gen.scala
@@ -1,6 +1,9 @@
 package chapter08
 
 import chapter06.{RNG, State}
+import chapter08.exercises.Exercise04
 
 case class Gen[+A](sample: State[RNG,A]) {
+
+  def choose(start: Int, stopExclusive: Int): Gen[Int] = Exercise04.choose(start,stopExclusive)
 }
--- src/chapter08/exercises/Exercise04.scala
+++ src/chapter08/exercises/Exercise04.scala
@@ -5,23 +5,17 @@ import chapter08.Gen
 
 object Exercise04 {
 
-  object Gen {
 
-    def choose(start: Int, stopExclusive: Int): Gen[Int] =
-      chapter08.Gen(State(RNG.nonNegativeInt).map(n => start + n % (stopExclusive - start)))
+  def choose(start: Int, stopExclusive: Int): Gen[Int] =
+    Gen(State(RNG.nonNegativeInt).map(n => start + n % (stopExclusive - start)))
 
-    /* We could write this as an explicit state action, but this is far less
-       convenient, since it requires us to manually thread the `RNG` through the
-       computation. */
-    def choose2(start: Int, stopExclusive: Int): Gen[Int] =
-      chapter08.Gen(State(rng => RNG.nonNegativeInt(rng) match {
-        case (n, rng2) => (start + n % (stopExclusive - start), rng2)
-      }))
-
-  }
-
-  def main(args: Array[String]) {
-    println("Gen.choose(1, 100) = " + Gen.choose(1, 100))
-  }
+  /* We could write this as an explicit state action, but this is far less
+     convenient, since it requires us to manually thread the `RNG` through the
+     computation. */
+  def choose2(start: Int, stopExclusive: Int): Gen[Int] =
+    Gen(State(rng => RNG.nonNegativeInt(rng) match {
+      case (n, rng2) => (start + n % (stopExclusive - start), rng2)
+    }))
 
 }
+
