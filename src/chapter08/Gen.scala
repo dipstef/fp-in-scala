511ef03c142f874325f46d547d13d36da0f7f03b
Stefano <stefano.dipierro@duedil.com>
2016-06-12 15:42:18 +0100

    Chapter 8 exercise 5

--- src/chapter08/Gen.scala
+++ src/chapter08/Gen.scala
@@ -1,9 +1,19 @@
 package chapter08
 
 import chapter06.{RNG, State}
-import chapter08.exercises.Exercise04
+import chapter08.exercises.{Exercise04, Exercise05}
 
-case class Gen[+A](sample: State[RNG,A]) {
+case class Gen[+A](sample: State[RNG, A]) {
 
-  def choose(start: Int, stopExclusive: Int): Gen[Int] = Exercise04.choose(start,stopExclusive)
+}
+
+object Gen {
+  def unit[A](a: => A): Gen[A] = Exercise05.unit(a)
+
+  def boolean: Gen[Boolean] = Exercise05.boolean
+
+  def choose(start: Int, stopExclusive: Int): Gen[Int] = Exercise04.choose(start, stopExclusive)
+
+  def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] = Exercise05.listOfN(n, g)
+
 }
