fb4b21e0847463ab72710e98f42f4a8a76e33ae8
Stefano <stefano.dipierro@duedil.com>
2016-06-12 21:52:28 +0100

    Updated example to use only non-empty lists

--- src/chapter08/Examples.scala
+++ src/chapter08/Examples.scala
@@ -1,16 +1,21 @@
 package chapter08
 
-import chapter08.Gen.listOf
+import chapter08.Gen.listOf1
 import chapter08.Prop.{forAll, run}
 
 object Examples {
 
-  def main(args: Array[String]) {
-    val smallInts = Gen.choose(-10, 10)
-
-    run(forAll(listOf(smallInts)) { ns =>
+  def testListMax(gen: Gen[Int]) = {
+    run(forAll(listOf1(gen)) { ns =>
       val max = ns.max
       !ns.exists(_ > max)
     })
+  }
+
+  def main(args: Array[String]) {
+    val smallInts = Gen.choose(-10, 10)
+
+    testListMax(smallInts)
+
   }
 }
