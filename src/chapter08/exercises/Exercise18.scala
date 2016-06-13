e8474b353ef4c1882d7ee9f91b24f2b6c5c9786c
Stefano <stefano.dipierro@duedil.com>
2016-06-13 01:46:10 +0100

    Chapter 8 exercise 18

---
+++ src/chapter08/exercises/Exercise18.scala
@@ -0,0 +1,14 @@
+package chapter08.exercises
+
+/**
+  Come up with some other properties that takeWhile should satisfy. Can you think of a good property expressing the
+  relationship between takeWhile and dropWhile?
+  */
+object Exercise18 {
+  /* `l.takeWhile(f) ++ l.dropWhile(f) == l`
+
+      We want to enforce that `takeWhile` returns the _longest_ prefix whose elements satisfy the predicate.
+      There are various ways to state this, but the general idea is that the remaining list, if non-empty,
+      should start with an element that does _not_ satisfy the predicate.
+  */
+}
