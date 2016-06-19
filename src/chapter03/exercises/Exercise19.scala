5ee1da54910650e8293cbda28ccbd7a0fbd93d7a
Stefano <stefano.dipierro@duedil.com>
2016-06-19 21:20:09 +0100

    Chapter 3 Exercise 19

---
+++ src/chapter03/exercises/Exercise19.scala
@@ -0,0 +1,27 @@
+package chapter03.exercises
+
+import chapter03.{Cons, List, Nil}
+import chapter03.List.foldRight
+
+import scala.collection.mutable.ListBuffer
+
+/**
+  * Write a function filter that removes elements from a list unless they satisfy a given predicate.
+  * Use it to remove all odd numbers from a List[Int].
+  */
+object Exercise19 {
+
+  def filter_[A](l: List[A])(f: A => Boolean): List[A] =
+    foldRight(l, Nil: List[A])((h, t) => if (f(h)) Cons(h, t) else t)
+
+  def filter[A](l: List[A])(f: A => Boolean): List[A] = {
+    val buf = new ListBuffer[A]
+    def go(l: List[A]): Unit = l match {
+      case Nil => ()
+      case Cons(h, t) => if (f(h)) buf += h; go(t)
+    }
+    go(l)
+    List(buf.toList: _*) // converting from the standard Scala list to the list we've defined here
+  }
+
+}
