06ec23ee818b4237bcc4c533674fc6a158e4a2ce
Stefano <stefano.dipierro@duedil.com>
2016-06-19 17:31:35 +0100

    Chapter 02 Exercise 05

---
+++ src/chapter02/exercise/Exercise05.scala
@@ -0,0 +1,10 @@
+package chapter02.exercise
+
+/**
+  * Implement the higher-order function that composes two functions.
+  */
+object Exercise05 {
+  def compose[A, B, C](f: B => C, g: A => B): A => C = {
+    a => f(g(a))
+  }
+}
