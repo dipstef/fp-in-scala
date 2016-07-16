0ae01f3dfd0ff31b074ad88f99fe7934202d313e
Stefano <stefano.dipierro@duedil.com>
2016-07-16 20:11:55 +0100

    doc formatting

--- src/chapter04/exercises/Exercise01.scala
+++ src/chapter04/exercises/Exercise01.scala
@@ -5,14 +5,14 @@ import scala.{Option => _, Some => _, Either => _, _}
 /**
   * Implement all of the preceding functions on Option. As you implement each function, try to think about what it means
   * and in what situations you’d use it. We’ll explore when to use each of these functions next.
-  * *
+  *
   * Here are a few hints for solving this exercise:
-  * *
+  *
   * It’s fine to use pattern matching, though you should be able to implement all the functions besides map and getOrElse
   * without resorting to pattern matching.
-  * *
+  *
   * For map and flatMap, the type signature should be enough to determine the implementation.
-  * *
+  *
   * getOrElse returns the result inside the Some case of the Option, or if the Option is None, returns the given default value.
   * orElse returns the first Option if it’s defined; otherwise, it returns the second Option.
   *
