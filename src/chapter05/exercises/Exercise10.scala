7ac106921dec455f7f62b1b5446c2d511cb8b65a
Stefano <stefano.dipierro@duedil.com>
2016-07-17 01:08:03 +0100

    chapter 5 exercise 10

--- src/chapter05/exercises/Exercise10.scala
+++ src/chapter05/exercises/Exercise10.scala
@@ -3,7 +3,6 @@ package chapter05.exercises
 import chapter05.Stream
 import chapter05.Stream.cons
 
-
 /**
   * Write a function fibs that generates the infinite stream of Fibonacci numbers: 0, 1, 1, 2, 3, 5, 8, and so on.
   */
