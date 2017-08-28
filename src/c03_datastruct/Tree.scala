a2f4870dc30ef6d136470bb51ba68a90a7e2b1f0
Stefano <stefano.dipierro@duedil.com>
2017-08-28 16:00:28 +0100

    renamed chapter packages

--- src/c03_datastruct/Tree.scala
+++ src/c03_datastruct/Tree.scala
@@ -1,6 +1,5 @@
 package c03_datastruct
 
-import c03_datastruct.exercises._
 
 sealed trait Tree[+A]
 
@@ -11,13 +10,34 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
 
-  def size[A](t: Tree[A]): Int = Exercise25.size(t)
+  // Exercise 25
+  def size[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 1
+    case Branch(l, r) => 1 + size(l) + size(r)
+  }
 
-  def maximum(t: Tree[Int]): Int = Exercise26.maximum(t)
+  // Exercise 26
+  def maximum(t: Tree[Int]): Int = t match {
+    case Leaf(n) => n
+    case Branch(l, r) => maximum(l) max maximum(r)
+  }
 
-  def depth[A](t: Tree[A]): Int = Exercise27.depth(t)
+  // Exercise 27
+  def depth[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 0
+    case Branch(l, r) => 1 + (depth(l) max depth(r))
+  }
 
-  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = Exercise28.map(t)(f)
+  // Exercise 28
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
+    case Leaf(a) => Leaf(f(a))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  }
 
-  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = Exercise29.fold(t)(f)(g)
+  // Exercise 29
+  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = t match {
+    case Leaf(a) => f(a)
+    case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
+  }
+
 }
